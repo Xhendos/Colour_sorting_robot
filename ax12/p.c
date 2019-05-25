4b78b2cd4df7dfce39efd988a6625c2e554bf04a
Max <521217@student.inholland.nl>
2019-05-25 02:48:55 +0200

    Add compare between goal and current position. Goal position is not read from shared memory but set instead by the process itself.

--- ax12/p.c
+++ ax12/p.c
@@ -13,16 +13,8 @@ typedef struct {
 	char *to;
 } instruction_t;
 
-typedef struct {
-	uint16_t goalPosition;
-} motor_t;
-
-typedef struct {
-	motor_t motors[6];
-} arm_t;
-
 static uint16_t currentPositions[48];
-static arm_t arms[8];
+static uint16_t goalPositions[48];
 
 void rotate(uint8_t arm, uint16_t aDegrees);
 void stretch(uint8_t arm, uint16_t bDegrees, uint16_t cDegrees, uint16_t dDegrees);
@@ -32,6 +24,30 @@ void setSpeed(uint8_t motor, uint16_t rpm);
 void setGoalPosition(uint8_t motor, uint16_t degrees);
 
 int main() {
+	//These are the wrong numbers.
+	//They should be in units instead of degrees.
+	//150 / 0.29 = 517.
+	goalPositions[0] = 150;
+	goalPositions[1] = 195;
+	goalPositions[2] = 60;
+	goalPositions[3] = 60;
+	goalPositions[4] = 150;
+	goalPositions[5] = 150;
+
+	goalPositions[6] = 150;
+	goalPositions[7] = 195;
+	goalPositions[8] = 60;
+	goalPositions[9] = 60;
+	goalPositions[10] = 150;
+	goalPositions[11] = 150;
+
+	goalPositions[42] = 150;
+	goalPositions[43] = 195;
+	goalPositions[44] = 60;
+	goalPositions[45] = 60;
+	goalPositions[46] = 150;
+	goalPositions[47] = 150;
+
 	instruction_t ins1 = {0, 1, 240, 60, 0, "t1", "t2"};
 	instruction_t ins2 = {0, 2, 240, 150, 0, "t2", "f2"};
 	instruction_t ins3 = {0, 8, 240, 60, 0, "t8", "t1"};
@@ -64,6 +80,16 @@ int main() {
 							|| strcmp(ins->to, prev_ins->to) == 0) {
 							allowed = 0;
 							break;
+						}
+					}
+				}
+
+				//Have all servo motors in the arm reached their goal position?
+				if (ins->state != 0) {
+					for (int j = 0; j < 6; ++j) {
+						uint8_t index = (ins->arm - 1) * 6 + j;
+						if (abs(currentPositions[index] - goalPositions[index]) > 5) {
+							//allowed = 0;
 						}
 					}
 				}
@@ -151,9 +177,11 @@ int main() {
 }
 
 void rotate(uint8_t arm, uint16_t aDegrees) {
+	uint8_t index = (arm - 1) * 6;
 	uint8_t aMotor = arm * 10 + 1;
 	setSpeed(aMotor, 15);
 	setGoalPosition(aMotor, aDegrees);
+	goalPositions[index] = aDegrees / 0.29;
 }
 
 void stretch(uint8_t arm, uint16_t bDegrees, uint16_t cDegrees, uint16_t dDegrees) {
@@ -173,22 +201,31 @@ void stretch(uint8_t arm, uint16_t bDegrees, uint16_t cDegrees, uint16_t dDegree
 	setGoalPosition(bMotor, bDegrees);
 	setGoalPosition(cMotor, cDegrees);
 	setGoalPosition(dMotor, dDegrees);
+	goalPositions[index+1] = bDegrees / 0.29;
+	goalPositions[index+2] = cDegrees / 0.29;
+	goalPositions[index+3] = dDegrees / 0.29;
 }
 
-void wrist(uint8_t arm, uint16_t degrees) {
+void wrist(uint8_t arm, uint16_t dDegrees) {
+	uint8_t index = (arm - 1) * 6;
 	uint8_t aMotor = arm * 10 + 4;
 	setSpeed(aMotor, 15);
-	setGoalPosition(aMotor, degrees);
+	setGoalPosition(aMotor, dDegrees);
+	goalPositions[index+3] = dDegrees / 0.29;
 }
 
 void claw(uint8_t arm, uint16_t eDegrees, uint16_t fDegrees) {
+	uint8_t index = (arm - 1) * 6;
 	uint8_t eMotor = arm * 10 + 5;
 	uint8_t fMotor = arm * 10 + 6;
 	setSpeed(eMotor, 15);
 	setSpeed(fMotor, 15);
 	setGoalPosition(eMotor, eDegrees);
 	setGoalPosition(fMotor, fDegrees);
+	goalPositions[index+4] = eDegrees / 0.29;
+	goalPositions[index+5] = fDegrees / 0.29;
 }
+
 
 //0--1023, 0.111 rpm per unit
 void setSpeed(uint8_t motor, uint16_t rpm) {
