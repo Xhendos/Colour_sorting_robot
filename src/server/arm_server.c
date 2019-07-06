035df19d695f52b933d5a6ecf1496b438ab30910
Max <521217@student.inholland.nl>
2019-07-06 14:11:53 +0200

    Apply FreeRTOS coding conventions. Add base for arm server task in source file.

--- src/server/arm_server.c
+++ src/server/arm_server.c
@@ -1,3 +1,20 @@
 #include "arm_server.h"
 
 
+void vTaskArmServer( void * pvParameters )
+{
+ArmServerMessage_t *pxMessage;
+
+    xArmServerMessageQueue = xQueueCreate(ARM_SERVER_MESSAGE_QUEUE_SIZE, sizeof(ArmServerMessage_t));
+
+    if ( xArmServerMessageQueue == NULL )
+    {
+        /* Message queue did not get created. */
+    }
+
+    while (1)
+    {
+
+    }
+}
+
--- src/server/arm_server.h
+++ src/server/arm_server.h
@@ -3,6 +3,9 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
+
+#define ARM_SERVER_MESSAGE_QUEUE_SIZE  ( 1 )
 
 #define ARM0        ( 1 << 0 )
 #define ARM1        ( 1 << 1 )
@@ -16,37 +19,41 @@
 #define ODD_ARMS    ( ARM1 | ARM3 | ARM5 | ARM7 )
 #define EVEN_ARMS   ( ARM0 | ARM2 | ARM4 | ARM6 )
 
-enum set_of_arms {
-    SET0 = ARM0,
-    SET1 = ARM1,
-    SET2 = ARM2,
-    SET3 = ARM3,
-    SET4 = ARM4,
-    SET5 = ARM5,
-    SET6 = ARM6,
-    SET7 = ARM7,
-    SET8 = ALL_ARMS,
-    SET9 = ODD_ARMS,
-    SET10 = EVEN_ARMS,
-};
+typedef enum {
+    eSet0 = ARM0,
+    eSet1 = ARM1,
+    eSet2 = ARM2,
+    eSet3 = ARM3,
+    eSet4 = ARM4,
+    eSet5 = ARM5,
+    eSet6 = ARM6,
+    eSet7 = ARM7,
+    eSet8 = ALL_ARMS,
+    eSet9 = ODD_ARMS,
+    eSet10 = EVEN_ARMS,
+} eSetOfArms;
 
-enum arm_movement {
-    EMPTY = 0,
-    REST_POSITION,
-    DISPLACE_60_150,
-    DISPLACE_60_240,
-    DISPLACE_150_60,
-    DISPLACE_150_240,
-    DISPLACE_240_60,
-    DISPLACE_240_150,
-};
+typedef enum {
+    eNoMovement = 0,
+    eRestPosition,
+    eDisplace60150,
+    eDisplace60240,
+    eDisplace15060,
+    eDisplace150240,
+    eDisplace24060,
+    eDisplace240150,
+} eArmMovement;
 
-struct arm_server_message {
-    enum set_of_arms arms;
-    enum arm_movement movement;
-    unsigned char buffer_movement;
-    TaskHandle_t sender_of_message;
-};
+typedef struct xARM_SEVER_MESSAGE {
+    eSetOfArms eArms;
+    eArmMovement eMovement;
+    unsigned char ucBufferMovement;
+    TaskHandle_t xSenderOfMessage;
+} ArmServerMessage_t;
+
+QueueHandle_t xArmServerMessageQueue;
+
+void vTaskArmServer( void * pvParameters );
 
 #endif /* _ARM_SERVER_H */
 
